6e5fdbee432ac2677fc66b8c1963e328a56571d1
Haady20 <149308447+Haady20@users.noreply.github.com>
2024-11-29 22:32:38 +0200

    Add files via upload

---
+++ AssigmentTwo Solution/AssigmentTwo/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace AssigmentTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+
+            #region Proplem 1
+            ////Intialize the variable x with a value 10
+            //int x = 10;
+
+            ////Intialize the variable y with a value 20
+            //int y = 20;
+
+            ///*Calculate the summation of x and y
+            //  and store the result as a value for the variable sum*/
+            //int sum = x + y;
+
+            ////write the value of sum as an output
+            //Console.WriteLine(sum);
+            #endregion
+
+            //------------------------------------------------------------
+
+            #region Proplem 2
+            ////Can't assign an string number to an integer
+            //int x = "10"; 
+            ////console shoud be Consolle
+            //console.WriteLine(x + y);
+            #endregion
+
+            //------------------------------------------------------------
+
+            #region Proplem 3
+            //string fullName = "Ahmed";
+            //int age = 19;
+            //double sallary = 2000; 
+            //bool student = false;
+            #endregion
+
+            //------------------------------------------------------------
+
+            #region Proplem 4
+            //int x = 15;
+            //int y = 4;
+
+            //int sum=   x + y;
+            //Console.WriteLine(sum);
+
+            //int diff = x - y;
+            //Console.WriteLine(diff);
+
+            //int pro = x * y;
+            //Console.WriteLine(pro);
+
+            //int div = x / y;
+            //Console.WriteLine(div);
+
+            //int remini = x % y;
+            //Console.WriteLine(remini);
+            #endregion
+
+            //------------------------------------------------------------
+
+            #region Proplem 5
+            //Console.WriteLine("Enter a number ...");
+
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //if (number > 10 && number % 2 == 0)
+            //{
+            //    Console.WriteLine("Number is greater than 10 and even!");
+            //}
+
+            //else if (number > 10)
+            //{
+            //    Console.WriteLine("Number is Greatter than 10.");
+            //}
+
+            //else if (number %2 == 0) {
+            //    Console.WriteLine("Number is Even.");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Number is not greater than 10 and not even!");
+            //}
+            #endregion
+
+            //------------------------------------------------------------
+
+            #region Proplem 6
+            //Console.WriteLine("Enter a double number...");
+
+            //double number = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("User input as double " + number);
+
+            //int explict=Convert.ToInt32(number);
+            //Console.WriteLine("User input as intergar " + explict);
+            #endregion
+
+            //------------------------------------------------------------
+
+            #region Proplem 7
+            //Console.WriteLine("Enter your age... "); 
+            //string age = Console.ReadLine();
+
+            //if (int.TryParse(age, out int ageee))
+            //{
+            //    if (ageee > 0)
+            //    {
+            //        Console.WriteLine("Your age is " + ageee);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid age!"); 
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Input!");
+            //}
+            #endregion
+
+            #region Proplem 8
+            //int x = 5;
+            //Console.WriteLine("X Postfix Result: " + x++);//5
+            //Console.WriteLine("X Prefix Result: " + ++x);//7
+            //int y = 10;
+            //Console.WriteLine("Y Postfix Result: " + y++);//10
+            //Console.WriteLine("Y Prefix Result: "+ ++y);//12
+            #endregion
+
+            //------------------------------------------------------------
+
+            #endregion
+        }
+    }
+        }
