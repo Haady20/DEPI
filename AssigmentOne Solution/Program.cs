3a8840f9996dd300513b2758d3927b97bcec5cee
Haady20 <149308447+Haady20@users.noreply.github.com>
2024-12-03 17:19:02 +0200

    Add files via upload

---
+++ AssigmentOne Solution/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AssigmentOne
+{
+    class Program
+
+    {
+     static void Main (){
+
+       
+ int x = 5;
+ int y = ++x + x++;
+ Console.WriteLine(y);
+
+     }  
+    }
+}
+
